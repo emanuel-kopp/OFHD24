ce8589572837ba92ed1e4e2ec16ac9c02b0d1010
Tschurr  Flavian <ftschurr@ethz.ch>
2024-03-23 11:01:30 +0100

    one bonitur update

--- app/scripts/history/plot_one_bonitur.R
+++ app/scripts/history/plot_one_bonitur.R
@@ -1,16 +1,17 @@
-make_example_plot_one_rating <- function(bonitur_ID) {
+make_example_plot_one_rating <- function(bonitur_ID_in) {
   library(ggplot2)
 
   
-  bonitur_ID <- c(1,1,1)
-  plot <- rep("Hinderegg", 3)
-  BBCH <- rep(60, 3)
-  pest <- c("Apfelwickler","Blutlaus","Gespinstmotte")
-  unit <- rep("Blütenbüschel", 3)
-  nr_of_units_counted <- rep(100, 3)
-  unit_infested <- c(25,3,20)
-  count_pests <- rep(NA, 3)
-  threshold <- c(20,12,22)
+  bonitur_ID <- c(1,1,1,2,2,2)
+  plot <- rep("Hinderegg", 6)
+  BBCH <- rep(60, 6)
+  pest <- c("Apfelwickler","Blutlaus","Gespinstmotte","Apfelwickler",
+            "Blutlaus","Gespinstmotte")
+  unit <- rep("Blütenbüschel", 6)
+  nr_of_units_counted <- rep(100, 6)
+  unit_infested <- c(25,3,20,12,6,30)
+  count_pests <- rep(NA, 6)
+  threshold <- c(20,12,22,20,12,22)
   
   df_sample <-
     data.frame(bonitur_ID,
@@ -22,7 +23,10 @@ make_example_plot_one_rating <- function(bonitur_ID) {
                unit_infested,
                count_pests,
                threshold)
+  
   df_sample$pest <- as.factor(df_sample$pest)
+  
+  df_sample <- subset(df_sample, bonitur_ID == bonitur_ID_in)
   
   ggplot(data = df_sample, aes(x = pest, y = unit_infested,fill=pest)) +
     geom_bar(stat = "identity", show.legend = F)+
